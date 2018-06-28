a944e60f7727537ce63e7223855cd3263ae9819f
jasonantao <jasantao99@gmail.com>
2018-06-27 23:16:01 -0400

    Update setup.sh
    Set environment command added for setEnv file

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #Setup File PLACEHOLDER
+. ./env/setEnv.sh
 echo "WordPress has now succesfully been installed and a database created."
 mysql USE mywpwebsite;
 echo "WordPress has now succesfully been installed and a database created."

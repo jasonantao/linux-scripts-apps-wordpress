7773f0f5fe7306d2e66fbadbd664369f4b7779f3
jasonantao <jasantao99@gmail.com>
2018-06-27 22:23:00 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,6 @@
 #Setup File PLACEHOLDER
 echo "WordPress has now succesfully been installed and a database created."
 
-mysql -u root
-
-mysql create database mywebsite;
-mysql grant usage on *.* to admin@localhost identified by 'password';
-mysql grant all privileges on mywebsite.* to admin@localhost;
-
-mysql use mywebsite;
-mysql exit;
+mysql -u root mywebsite < cmds.txt
 
 echo "WordPress has now succesfully been installed and a database created."

a2da87897054599e056830cc366c8b2cba70bac8
jasonantao <jasantao99@gmail.com>
2018-06-27 22:08:01 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,5 +6,8 @@ mysql create database mywebsite;
 mysql grant usage on *.* to admin@localhost identified by 'password';
 mysql grant all privileges on mywebsite.* to admin@localhost;
 
+mysql grant all privileges on mywebsite.* to admin@localhost;
+
+
 mysql use mywebsite;
 mysql exit;

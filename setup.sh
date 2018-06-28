44cf53953e3a8358ef7be4dff0e7f1d46aff6d1a
jasonantao <jasantao99@gmail.com>
2018-06-27 21:46:25 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,11 @@
 #Setup File PLACEHOLDER
+
+mysql -u root
+
+
+create database website;
+grant usage on *.* to admin@localhost identified by 'password';
+grant all privileges on website.* to admin@localhost;
+
+use website;
+exit;

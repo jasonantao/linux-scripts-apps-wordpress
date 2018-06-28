192001e471013823dfb2f62dc357ecf2ef6c1682
jasonantao <jasantao99@gmail.com>
2018-06-27 23:08:02 -0400

    Update freshwordpressfile.sh
    Necessary comments, spacing, and documentation

--- freshwordpressfile.sh
+++ freshwordpressfile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Setting reference to the current directory
 sqlCurrDir=$PWD
 
 # Ensure script is running under root
@@ -8,19 +9,23 @@ if [ "$EUID" -ne 0 ]
   return -1
 fi
 
-
+#Generating directory for which website and wordpress will be installed
 cd $HOME
 mkdir /var/tmp/www/
 cd /var/tmp/www/
 
+#Getting wordpress latest tar file
 wget http://wordpress.org/latest.tar.gz
 tar xfz latest.tar.gz
+
+#Moving wordpress files back one step in the directory
 mv wordpress/* ./
 
-
+#Removing unecessary files from the fore mentioned file shift
 rmdir ./wordpress/
 rm -f latest.tar.gz
 
+#Git initialization installation
 yum install git -y
 
 #Set Cloning Properties
@@ -46,11 +51,17 @@ find . -name "*sh" -exec chmod 700 {} \;
 
 
 # Setup Project
+#Note: Do not, and cannot use setup.sh in this case to run .sql file with SQL commands
 #./setup.sh
-#mysql -u root < cmds.txt;
+
+#Logging into mysql to run commands
 mysql -u root
+
+#Creating database mywpwebsite and setting it to be used with running commands
 create database mywpwebsite;
 USE mywpwebsite;
+
+#Pulling sql commands from .sql file cmds to which commands in sql are run to create wordpress ready database and exit mysql
 source /tmp/wordpress/cmds.sql
 exit;
 

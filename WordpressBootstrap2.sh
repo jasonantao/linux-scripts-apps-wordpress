0e0526139b50ad9eca1e10d869d94a9414ec2bc0
jasonantao <jasantao99@gmail.com>
2018-07-02 12:56:24 -0400

    Update WordpressBootstrap2.sh

--- WordpressBootstrap2.sh
+++ WordpressBootstrap2.sh
@@ -9,10 +9,13 @@ if [ "$EUID" -ne 0 ]
   return -1
 fi
 
-yum install httpd
+#Installing httpd and starting service.
+yum install httpd -y
 service httpd start
 
-yum install php php-mysql
+#Installing php-mysql
+#In order to take effect, need a restart on httpd service
+yum install php php-mysql -y
 service httpd restart
 
 #Git initialization installation
@@ -32,23 +35,31 @@ fi
 # Clone $pkg
 echo Executing $clone$gitRepo $installDir
 $clone$gitRepo $installDir
+
 # Setup $pkg
 cd $installDir
+
 # MAKE ALL SHELL SCRIPTS EXECUTABLE TO ROOT ONLY
 find . -name "*sh" -exec chmod 700 {} \;
 
+#Setup for Wordpress Installation
+#Directory setup and file installation setup and management
 cd /var/www/html
 wget http://wordpress.org/latest.tar.gz
 tar -xzvf latest.tar.gz
 
+#Initial message - will repeat
 echo "Wordpress Installation Complete"
-echo "Go to http://(your_ip_adress)/wordpress to launch your blank wordpress site.
-
+echo "Go to http://your_ip_adress/wordpress to launch your blank wordpress site.
 
 #Pulling sql commands from .sql file cmds to which commands in sql are run to create wordpress ready database and exit mysql
+#NOTE: MYSQL FIRST CAN TO BE INSTALLED
+#NOT REQUIRED, BUT IF IT IS, WILL JUST SKIP A STEP IN WORDPRESS LOGIN SETUP PRELIMS
+#THIS WAY, THIS SCRIPT WORKS NO MATTER WHICH LINUX INSTANCE YOU LAUNCH!!!!!
+mysql -u root
 source /tmp/wordpress/wpcmds.sql
 exit;
 
 cd $HOME
 echo "Wordpress Installation Complete"
-echo "Go to http://(your_ip_adress)/wordpress to launch your blank wordpress site.
+echo "Go to http://your_ip_adress/wordpress to launch your blank wordpress site.

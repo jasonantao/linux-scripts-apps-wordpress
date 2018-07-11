fdc9a401c37408d3607f57c995b586c649b480f0
jasonantao <jasantao99@gmail.com>
2018-07-11 19:21:24 -0400

    Update WordpressBootstrap.sh

--- WordpressBootstrap.sh
+++ WordpressBootstrap.sh
@@ -67,8 +67,12 @@ echo "Go to http://your_ip_adress/wordpress to launch your blank wordpress site.
 #NOTE: MYSQL FIRST CAN TO BE INSTALLED
 #NOT REQUIRED, BUT IF IT IS, WILL JUST SKIP A STEP IN WORDPRESS LOGIN SETUP PRELIMS
 #THIS WAY, THIS SCRIPT WORKS NO MATTER WHICH LINUX INSTANCE YOU LAUNCH!!!!!
+rpm -qa | grep mysql
+yum install mysql-server -y
+service mysqld status
+service mysqld start
 
 mysql -u root
 source /tmp/wordpress/install/wpcmds.sql
-
+exit;
 cd $HOME

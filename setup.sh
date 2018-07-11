a2245f430987505df0ccc6e10b3a920d61aae240
jasonantao <jasantao99@gmail.com>
2018-07-11 19:49:25 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,26 @@
 #Setup File PLACEHOLDER
-. ./env/setEnv.sh
-echo "WordPress has now succesfully been installed and a database created."
-mysql USE mywpwebsite;
-echo "WordPress has now succesfully been installed and a database created."
+
+#####################################################################################
+#Setup for Wordpress Installation
+#Directory setup and file installation setup and management
+
+cd /var/www/html
+wget http://wordpress.org/latest.tar.gz
+tar -xzvf latest.tar.gz
+
+#Initial message - will repeat
+echo "Wordpress Installation Complete"
+echo "Go to http://your_ip_adress/wordpress to launch your blank wordpress site."
+
+#Pulling sql commands from .sql file cmds to which commands in sql are run to create wordpress ready database and exit mysql
+#NOTE: MYSQL FIRST CAN TO BE INSTALLED
+#NOT REQUIRED, BUT IF IT IS, WILL JUST SKIP A STEP IN WORDPRESS LOGIN SETUP PRELIMS
+#THIS WAY, THIS SCRIPT WORKS NO MATTER WHICH LINUX INSTANCE YOU LAUNCH!!!!!
+rpm -qa | grep mysql
+yum install mysql-server -y
+service mysqld status
+service mysqld start
+
+mysql -u root
+source /tmp/wordpress/install/wpcmds.sql
+exit;

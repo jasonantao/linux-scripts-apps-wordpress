be2d4fd50b7d51f57fdd4acd06d39173dc3d3255
jasonantao <jasantao99@gmail.com>
2018-07-20 20:11:09 -0400

    Create installwordpress.sh

---
+++ install/installwordpress.sh
@@ -0,0 +1,7 @@
+cd /var/www/html
+wget http://wordpress.org/latest.tar.gz
+tar -xzvf latest.tar.gz
+
+#Initial message - will repeat
+echo "Wordpress Installation Complete"
+echo "Go to http://your_ip_adress/wordpress to launch your blank wordpress site."

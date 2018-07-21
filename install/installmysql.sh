19d4e5dac45028e94bb842d87d6a95671cf375dd
jasonantao <jasantao99@gmail.com>
2018-07-20 20:28:22 -0400

    Update installmysql.sh
    #Pulling sql commands from .sql file cmds to which commands in sql are run to create wordpress ready database and exit mysql
    #NOTE: MYSQL FIRST CAN TO BE INSTALLED
    #NOT REQUIRED, BUT IF IT IS, WILL JUST SKIP A STEP IN WORDPRESS LOGIN SETUP PRELIMS
    #THIS WAY, THIS SCRIPT WORKS NO MATTER WHICH LINUX INSTANCE YOU LAUNCH!!!!!

--- install/installmysql.sh
+++ install/installmysql.sh
@@ -1,7 +1,4 @@
-#Pulling sql commands from .sql file cmds to which commands in sql are run to create wordpress ready database and exit mysql
-#NOTE: MYSQL FIRST CAN TO BE INSTALLED
-#NOT REQUIRED, BUT IF IT IS, WILL JUST SKIP A STEP IN WORDPRESS LOGIN SETUP PRELIMS
-#THIS WAY, THIS SCRIPT WORKS NO MATTER WHICH LINUX INSTANCE YOU LAUNCH!!!!!
+#Install MySQL
 echo 'Installing mysql server'
 rpm -qa | grep mysql
 yum install mysql-server -y

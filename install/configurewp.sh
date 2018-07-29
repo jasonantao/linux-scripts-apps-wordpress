b69042bacf51b2e73a45f32e06cd1ede451f7672
jasonantao <jasantao99@gmail.com>
2018-07-29 17:57:37 -0400

    Update configurewp.sh

--- install/configurewp.sh
+++ install/configurewp.sh
@@ -1,7 +1,7 @@
 #Configure WordPress
 
 ###################### Testing below ###########################
-
+wpConfigDir=$PWD
 cd /tmp/wordpress/install/
 cp wp-config.php /var/www/html/wordpress/
 cd /var/www/html/wordpress/
@@ -12,4 +12,6 @@ CREATE DATABASE jason;
 GRANT ALL PRIVILEGES ON jason.* TO "jason"@"localhost" IDENTIFIED BY "jason";
 FLUSH PRIVILEGES;
 exit;
+
+cd wpConfigDir
 

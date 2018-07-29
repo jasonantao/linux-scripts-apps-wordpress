8a0d4a2e0dcf88d6770df9ebc6114754622f1d1d
jasonantao <jasantao99@gmail.com>
2018-07-29 18:59:41 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,9 +6,12 @@
 
 #Install WordPress
 . ./install/installwordpress.sh
+echo HERE 1========================= After . ./install/installwordpress.sh PWD = $PWD ====================================
 
 #Install MySQL
 . ./install/installmysql.sh
+echo HERE 2========================= After . ./install/installmysql.sh = $PWD ====================================
 
 #Configure WordPress
 . ./install/configurewp.sh
+echo HERE 3========================= After . ./install/configurewp.sh PWD = $PWD ====================================

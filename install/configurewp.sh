3209e011289bf8f715922a2e7028a78070e90960
jasonantao <jasantao99@gmail.com>
2018-07-28 21:30:36 -0400

    Update configurewp.sh

--- install/configurewp.sh
+++ install/configurewp.sh
@@ -1,2 +1,15 @@
 #Configure WordPress
-#IN PROGRESS
+
+###################### Testing below ###########################
+
+cd /tmp/wordpress/install/
+cp wp-config.php /var/www/html/wordpress/
+cd /var/www/html/wordpress/
+chmod 777 wp-config.php
+
+mysql -u root
+CREATE DATABASE jason;
+GRANT ALL PRIVILEGES ON jason.* TO "jason"@"localhost" IDENTIFIED BY "jason";
+FLUSH PRIVILEGES;
+exit;
+

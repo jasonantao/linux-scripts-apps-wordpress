3a774933d1d8902c6f5e0c52e4f64dde032bda72
jasonantao <jasantao99@gmail.com>
2018-07-28 21:30:09 -0400

    Update WordpressBootstrap.sh

--- WordpressBootstrap.sh
+++ WordpressBootstrap.sh
@@ -41,16 +41,3 @@ cd $installDir
 
 cd $wpCurrDir
 
-###################### Testing below ###########################
-
-cd /tmp/wordpress/install/
-cp wp-config.php /var/www/html/wordpress/
-cd /var/www/html/wordpress/
-chmod 777 wp-config.php
-
-mysql -u root
-CREATE DATABASE jason;
-GRANT ALL PRIVILEGES ON jason.* TO "jason"@"localhost" IDENTIFIED BY "jason";
-FLUSH PRIVILEGES;
-exit;
-

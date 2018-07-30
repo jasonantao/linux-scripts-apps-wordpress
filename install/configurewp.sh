ddd0e23d50b45fe9d63fecf25c521896d8847749
jasonantao <jasantao99@gmail.com>
2018-07-30 15:11:13 -0400

    Update configurewp.sh

--- install/configurewp.sh
+++ install/configurewp.sh
@@ -7,12 +7,14 @@ cd /tmp/wordpress/install/
 cp wp-config.php /var/www/html/wordpress/
 cd /var/www/html/wordpress/
 chmod 777 wp-config.php
+echo ===================== 'About to Start MySQL' ===================================
 
 mysql -u root
 CREATE DATABASE jason;
 GRANT ALL PRIVILEGES ON jason.* TO "jason"@"localhost" IDENTIFIED BY "jason";
 FLUSH PRIVILEGES;
 exit;
+echo ===================== 'Done MySQL' ===================================
 
 cd wpConfigDir
 

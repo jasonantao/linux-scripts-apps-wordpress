e6f4b4b35eb80dbaa8622d43babfe31b5a398501
Jason <jasantao99@gmail.com>
2019-09-26 22:12:14 -0400

    PHP Required Update to Latest Site Standards

--- install/installphpmysql.sh
+++ install/installphpmysql.sh
@@ -1,5 +1,6 @@
 #Install php-mysql
 echo 'Installing php-mysql'
 #In order to take effect, need a restart on httpd service
-yum install php php-mysql -y
+#yum install php php-mysql -y
+amazon-linux-extras install php7.2
 service httpd restart

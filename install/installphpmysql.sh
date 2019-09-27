beb9865dc9cdafaa65610e58719d0efe2f6c726e
Jason <jasantao99@gmail.com>
2019-09-26 23:00:35 -0400

    Reverted to latest PHP standards

--- install/installphpmysql.sh
+++ install/installphpmysql.sh
@@ -2,6 +2,5 @@
 echo 'Installing php-mysql'
 #In order to take effect, need a restart on httpd service
 #yum install php php-mysql -y
-yum install -y php73 php73-devel php73-mysql php73-pdo php73-mbstring
-yum install -y php73-gd php73-xml php73-xmlrpc php73-mcrypt php-pear
+amazon-linux-extras install php7.2 -y
 service httpd restart

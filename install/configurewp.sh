25782521601b3de1a6a531fcc9466943fc45fe96
Jason <jasantao99@gmail.com>
2019-09-28 16:13:44 -0400

    New Fix

--- install/configurewp.sh
+++ install/configurewp.sh
@@ -7,7 +7,7 @@ cd /var/www/html/wordpress/
 chmod 777 wp-config.php
 
 mysql -u root -e "CREATE DATABASE wpconfig"
-mysql -u root -D wpconfig -e "GRANT ALL PRIVILEGES ON wpconfig.* TO 'jason'@'localhost' IDENTIFIED BY 'david'"
+mysql -u root -D wpconfig -e "GRANT ALL PRIVILEGES ON wpconfig.* TO 'wpconfig'@'localhost' IDENTIFIED BY 'david'"
 mysql -u root -e "FLUSH PRIVILEGES"
 exit;
 

1862b3e60a4f4066599586fc4df85bf2207db71f
Jason <jasantao99@gmail.com>
2019-09-28 22:11:06 -0400

    Full Site Configuration Capability v1 Measures

--- install/configurewp.sh
+++ install/configurewp.sh
@@ -2,12 +2,19 @@
 
 wpConfigDir=$PWD
 cd /$installDir/install/
-#cp wp-config.php /var/www/html/wordpress/
+cp wp-config.php /var/www/html/wordpress/
 cd /var/www/html/wordpress/
-#chmod 777 wp-config.php
+chmod 777 wp-config.php
+
+#Can now change wp-config.php here
+#sed -i  's/DB_NAME/jasontest/' wp-config.php - This is the format of sed commands to use
+sed -i  "s/wpconfig1/${dbName}/" wp-config.php
+sed -i  "s/wpconfig2/${userName}/" wp-config.php
+sed -i  "s/wpconfig3/${dbpwd}/" wp-config.php
+
 
 mysql -u root -e "CREATE DATABASE ${dbName}"
-mysql -u root -D $dbName -e "GRANT ALL PRIVILEGES ON ${dbName}.* TO '${dbName}'@'localhost' IDENTIFIED BY '${dbName}'"
+mysql -u root -D $dbName -e "GRANT ALL PRIVILEGES ON ${dbName}.* TO '${userName}'@'localhost' IDENTIFIED BY '${dbpwd}'"
 mysql -u root -e "FLUSH PRIVILEGES"
 exit;
 
--- install/wp-config.php
+++ install/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wpconfig');
+define('DB_NAME', 'wpconfig1');
 
 /** MySQL database username */
-define('DB_USER', 'wpconfig');
+define('DB_USER', 'wpconfig2');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'wpconfig');
+define('DB_PASSWORD', 'wpconfig3');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');

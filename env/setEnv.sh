885f1573c922a3694ba2f962d5fd4828fb738158
Jason <jasantao99@gmail.com>
2019-09-29 16:58:31 -0400

    Subnested Exports Handling

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -6,10 +6,4 @@ mySqlGroup=mySql
 mySqlHome=/opt/mySql
 mySqlLog=/var/log/mySql
 
-#MySQL Params
-echo 'The dbName is: ' $1
-export dbName=$1
-echo 'The userName is: ' $2
-export userName=$2
-echo 'The dbpwd is: ' $3
-export dbpwd=$3
+#NOTE - THIS IS A SUB-NESTED LAYER AND SO EXPORTS MADE HERE WILL NOT BE AVAILABLE IN THE INDIVIDUAL INSTALL SCRIPTS

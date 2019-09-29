0bf9d2de3f8ea23ed9121e4bda2841fc528f69f9
Jason <jasantao99@gmail.com>
2019-09-28 22:29:42 -0400

    UPdate

--- install/configurewp.sh
+++ install/configurewp.sh
@@ -2,10 +2,12 @@
 
 wpConfigDir=$PWD
 cd /$installDir/install/
-echo "--------------------------------------------------------- HERE --------------------------------------------------"
+echo "--------------------------------------------------------- HERE ONE -----------------------------------------------------"
 cp wp-config.php /var/www/html/wordpress/
 cd /var/www/html/wordpress/
 chmod 777 wp-config.php
+echo "----------------------------------------------------- HERE TWO ----------------------------------------------------"
+
 
 #Can now change wp-config.php here
 #sed -i  's/DB_NAME/jasontest/' wp-config.php - This is the format of sed commands to use
@@ -14,7 +16,7 @@ sed -i  "s/wpconfig2/${userName}/" wp-config.php
 sed -i  "s/wpconfig3/${dbpwd}/" wp-config.php
 
 
-mysql -u root -e "CREATE DATABASE ${dbName}";
+mysql -u root -e "CREATE DATABASE ${dbName}"
 mysql -u root -D $dbName -e "GRANT ALL PRIVILEGES ON ${dbName}.* TO '${userName}'@'localhost' IDENTIFIED BY '${dbpwd}'"
 mysql -u root -e "FLUSH PRIVILEGES"
 exit;

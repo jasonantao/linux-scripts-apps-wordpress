174a3f35a89f2852b4a6ba5d546b0e80beef2ced
Jason <jasantao99@gmail.com>
2019-09-28 21:44:19 -0400

    Another temporary change

--- install/configurewp.sh
+++ install/configurewp.sh
@@ -4,7 +4,7 @@ wpConfigDir=$PWD
 cd /$installDir/install/
 #cp wp-config.php /var/www/html/wordpress/
 cd /var/www/html/wordpress/
-chmod 777 wp-config.php
+#chmod 777 wp-config.php
 
 mysql -u root -e "CREATE DATABASE ${dbName}"
 mysql -u root -D $dbName -e "GRANT ALL PRIVILEGES ON ${dbName}.* TO '${dbName}'@'localhost' IDENTIFIED BY '${dbName}'"

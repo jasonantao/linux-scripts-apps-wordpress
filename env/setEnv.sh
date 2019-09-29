d993f43980cb86db1d5fce3cdd729a9f7defc8ac
Jason <jasantao99@gmail.com>
2019-09-29 17:57:36 -0400

    Update to environment

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -7,3 +7,10 @@ mySqlHome=/opt/mySql
 mySqlLog=/var/log/mySql
 
 #NOTE - THIS IS A SUB-NESTED LAYER AND SO EXPORTS MADE HERE WILL NOT BE AVAILABLE IN THE INDIVIDUAL INSTALL SCRIPTS
+
+echo Setting External Args
+echo These Arguments Overwrite Default Argument Settings
+for arg in "$@"; do
+  echo setArgs EXECUTING: export $arg
+  export $arg
+done

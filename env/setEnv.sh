e4d644aa2b179555a4bd7ccc2a9b83d7bd88e227
Jason <jasantao99@gmail.com>
2019-10-31 22:30:14 -0400

    Environment Variables Added

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -7,6 +7,11 @@ mySqlHome=/opt/mySql
 mySqlLog=/var/log/mySql
 
 #NOTE - THIS IS A SUB-NESTED LAYER AND SO EXPORTS MADE HERE WILL NOT BE AVAILABLE IN THE INDIVIDUAL INSTALL SCRIPTS
+dbName=admin
+userName=admin
+dbpwd=pass
+sitename=cloudinitializer
+
 
 echo Setting External Args
 echo These Arguments Overwrite Default Argument Settings
